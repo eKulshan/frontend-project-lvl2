c01ebef6e4202e69287aa0efb0c08ff9dfd2bc1d
eKulshan <eikulshan@gmail.com>
2020-06-22 14:46:28 +0300

    add arguments & format option, step 2 complited

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ program
     .name('gendiff')
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format')
     .action(()=>{
         program.help();
     });
+    
 program.parse(process.argv);
     
 export default program;
