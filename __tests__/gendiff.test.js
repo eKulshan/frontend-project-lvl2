1ef6d99aea26045e0b40972459405698a2563221
eKulshan <eikulshan@gmail.com>
2020-07-13 20:50:10 +0300

    done tests refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,26 +1,26 @@
 import genDiff from '../src/index.js';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import readFile from '../src/readFile.js';
+import { readFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-console.log(__dirname);
 const getFixturePath = (filename) => path.join(__dirname, '.', '__fixtures__', filename);
 
-test.each([
-  ['json', 'stylish', 'stylishExpected.txt'],
-  ['yml',  'stylish', 'stylishExpected.txt'],
-  ['ini',  'stylish', 'stylishExpected.txt'],
-  ['json', 'plain', 'plainExpected.txt'],
-  ['yml',  'plain', 'plainExpected.txt'],
-  ['ini',  'plain', 'plainExpected.txt'],
-  ['json', 'json', 'jsonExpected.json'],
-  ['yml',  'json', 'jsonExpected.json'],
-  ['ini',  'json', 'jsonExpected.json'],
+const extensions = ['json', 'yml', 'ini'];
+const formats = ['stylish', 'plain', 'json'];
+let expected;
+beforeAll(() => {
+  expected = {};
+  expected.stylish = readFileSync(getFixturePath(`expected_stylish.txt`), 'utf-8');
+  expected.plain = readFileSync(getFixturePath(`expected_plain.txt`), 'utf-8');
+  expected.json = readFileSync(getFixturePath(`expected_json.json`), 'utf-8');
+})
 
-])('gendiff %s output match', (extension, format, expectedOupput) => {
-  expect(genDiff(getFixturePath(`before.${extension}`), getFixturePath(`after.${extension}`), format)).toEqual(readFile(getFixturePath(expectedOupput))[0]);
+describe.each(extensions)('gendiff %s output match', (extension) => {
+  test.each(formats)('%s format output match', (format) => {
+    const path1 = getFixturePath(`file1.${extension}`);
+    const path2 = getFixturePath(`file2.${extension}`);
+    expect(genDiff(path1, path2, format)).toEqual(expected[format]);
+  })
 });
-
-
