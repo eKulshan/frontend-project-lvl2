4d0c8ca0b487f700040a53b088640da11a5422ef
eKulshan <eikulshan@gmail.com>
2020-08-11 11:22:52 +0300

    minor changes in

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -9,26 +9,26 @@ const formatValue = (data) => {
 };
 
 const makePlain = (diffTree) => {
-  const buildOutput = (data, paths) => data.reduce((acc, {
+  const buildOutput = (data, paths) => data.flatMap(({
     key, status, oldValue, newValue, children,
   }) => {
     const currPath = [...paths, key];
     const fullName = currPath.join('.');
     switch (status) {
       case 'removed':
-        return [...acc, `Property '${fullName}' was removed`];
+        return `Property '${fullName}' was removed`;
       case 'added':
-        return [...acc, `Property '${fullName}' was added with value: ${formatValue(newValue)}`];
+        return `Property '${fullName}' was added with value: ${formatValue(newValue)}`;
       case 'unchanged':
-        return acc;
+        return [];
       case 'changed':
-        return [...acc, `Property '${fullName}' was updated. From ${formatValue(oldValue)} to ${formatValue(newValue)}`];
+        return `Property '${fullName}' was updated. From ${formatValue(oldValue)} to ${formatValue(newValue)}`;
       case 'nested':
-        return [...acc, buildOutput(children, currPath)];
+        return buildOutput(children, currPath);
       default:
         throw new Error(`${status} is unknown status!`);
     }
-  }, []).join('\n');
+  }).join('\n');
   return buildOutput(diffTree, []);
 };
 
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -11,35 +11,31 @@ const formatValue = (data, depth) => {
   if (!_.isObject(data)) {
     return data;
   }
-  const formatedValue = Object.entries(data).reduce((acc, [key, value]) => [...acc, `${currIndent}      ${key}: ${formatValue(value, depth + 1)}`], []);
+  const formatedValue = Object.entries(data).map(([key, value]) => `${currIndent}      ${key}: ${formatValue(value, depth + 1)}`);
   return `{\n${formatedValue.join('\n')}\n${currIndent}  }`;
 };
 
 const makeStylish = (diffTree) => {
   const buildOutput = (data, depth = 0) => {
     const currIndent = getIndent(depth);
-    return data.reduce((acc, {
+    return data.map(({
       key, status, oldValue, newValue, value, children,
     }) => {
       switch (status) {
         case 'removed':
-          return [...acc, `${currIndent}- ${key}: ${formatValue(oldValue, depth)}`];
+          return `${currIndent}- ${key}: ${formatValue(oldValue, depth)}`;
         case 'added':
-          return [...acc, `${currIndent}+ ${key}: ${formatValue(newValue, depth)}`];
+          return `${currIndent}+ ${key}: ${formatValue(newValue, depth)}`;
         case 'unchanged':
-          return [...acc, `${currIndent}  ${key}: ${formatValue(value, depth)}`];
+          return `${currIndent}  ${key}: ${formatValue(value, depth)}`;
         case 'changed':
-          return [
-            ...acc,
-            `${currIndent}+ ${key}: ${formatValue(oldValue, depth)}`,
-            `${currIndent}- ${key}: ${formatValue(newValue, depth)}`,
-          ];
+          return `${currIndent}+ ${key}: ${formatValue(oldValue, depth)}\n${currIndent}- ${key}: ${formatValue(newValue, depth)}`;
         case 'nested':
-          return [...acc, `${currIndent}  ${key}: {\n${buildOutput(children, depth + 1)}\n${currIndent}  }`];
+          return `${currIndent}  ${key}: {\n${buildOutput(children, depth + 1)}\n${currIndent}  }`;
         default:
           throw new Error(`${status} is unknown status!`);
       }
-    }, []).join('\n');
+    }).join('\n');
   };
   return `{\n${buildOutput(diffTree)}\n}`;
 };
